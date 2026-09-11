21692384b8092efe3c6ffa9743ee1844dfa0bde2
Satyam Sharma <161165084+satyamsharma58025@users.noreply.github.com>
2026-09-11 14:05:15 +0530

    feat: Add view configuration file for Blade template paths and compiled view storage

---
+++ public_html (3)/config/view.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | View Storage Paths
+    |--------------------------------------------------------------------------
+    |
+    | Most of the views for the application are stored in the resources/views
+    | directory. You may add additional paths here as needed.
+    |
+    */
+
+    'paths' => [
+        resource_path('views'),
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Compiled View Path
+    |--------------------------------------------------------------------------
+    |
+    | This option determines where compiled Blade templates are stored.
+    |
+    */
+
+    'compiled' => env(
+        'VIEW_COMPILED_PATH',
+        realpath(storage_path('framework/views'))
+    ),
+
+];
